0ade88bf67fb2f4369df00ebc80b14ddc4a0d9ed
shay <43248357+shayypy@users.noreply.github.com>
2025-01-12 08:39:29 -0600

    chore: remove admin ping in landing message

--- src/commands/application.ts
+++ src/commands/application.ts
@@ -28,10 +28,10 @@ module.exports = {
     ]);
     await channel.send({
       content: [
-        "Be sure to read the welcome message! If you just skipped by it, that's fine, press the button to get started.",
-        "",
-        "If you have questions, <#911031775302672424> is the place to ask. You can also DM <@404380636833644557> for a potentially quicker response.",
-      ].join("\n"),
+        "Be sure to read the welcome message! If you just skipped by it, ",
+        "that's fine, press the button to get started. If you have questions, ",
+        "<#911031775302672424> is the place to ask.",
+      ].join(" "),
       components: [
         new ActionRowBuilder<ButtonBuilder>().addComponents(
           new ButtonBuilder()
